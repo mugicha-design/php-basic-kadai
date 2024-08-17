e410e872e3cbd48e8ad8bf777081732b0c55e225
mugicha-design <167395735+mugicha-design@users.noreply.github.com>
2024-08-18 00:15:35 +0900

    kadai_007をアップロードしました

---
+++ htdocs/kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>kadai_007</title>
+</head>
+
+<body>
+    <p>
+        <?php
+            $price = ['name' => 'onion' , 'price' => 200 , 'weight' => 160];
+            print_r ($price);
+        ?>
+    </p>
+</body>
+
+</html>
